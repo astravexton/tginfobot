b04e5301b940c5bc270713389f503479e3553406
Astra <squ1dge08@gmail.com>
2020-04-30 17:35:24 +0100

    Init

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/astravexton/tginfobot
+
+go 1.14
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
+	github.com/sasbury/mini v0.0.0-20181226232755-dc74af49394b
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+	gopkg.in/tucnak/telebot.v2 v2.0.0-20200426184946-59629fe0483e // indirect
+)
---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strings"
+	"time"
+
+	"github.com/sasbury/mini"
+	tb "gopkg.in/tucnak/telebot.v2"
+)
+
+func checkErr(e error) {
+	if e != nil {
+		log.Fatal(e)
+	}
+}
+
+func main() {
+	config, err := mini.LoadConfiguration("config.ini")
+	checkErr(err)
+
+	b, err := tb.NewBot(tb.Settings{
+		Token:  config.StringFromSection("bot", "token", ""),
+		Poller: &tb.LongPoller{Timeout: 10 * time.Second},
+	})
+
+	checkErr(err)
+
+	b.Handle(tb.OnUserJoined, func(m *tb.Message) {
+		var msg = "\\[Gearheads\\]\nNew member\n\n👤"
+		var name = fmt.Sprintf("[%s", escapeMarkdownChar(m.UserJoined.FirstName))
+		if m.UserJoined.LastName != "" {
+			name = fmt.Sprintf("%s %s](tg://user?id=%s)", name, escapeMarkdownChar(m.UserJoined.LastName), m.UserJoined.Recipient())
+		} else {
+			name = fmt.Sprintf("%s](tg://user?id=%s)", name, m.UserJoined.Recipient())
+		}
+
+		if m.UserJoined.Username != "" {
+			msg = fmt.Sprintf("%s %s \\(@%s\\)", msg, name, m.UserJoined.Username)
+		}
+
+		msg = fmt.Sprintf("%s\n\\#new\\_member \\#c%s \\#u%s", msg, m.Chat.Recipient()[4:], m.UserJoined.Recipient())
+
+		b.Send(&tb.Chat{ID: config.IntegerFromSection("bot", "logchannel", 0)}, msg, &tb.SendOptions{ParseMode: tb.ModeMarkdownV2})
+	})
+
+	b.Start()
+
+}
+
+func escapeMarkdownChar(s string) string {
+	replacer := strings.NewReplacer("*", "\\*", "_", "\\_", "[", "\\[", "`", "\\`")
+	return replacer.Replace(s)
+}
