bdab2acec62e39af1313a524737936e369b5c4ce
Astra <squ1dge08@gmail.com>
2020-05-17 21:18:57 +0100

    Add debug logging

--- main.go
+++ main.go
@@ -28,21 +28,27 @@ func main() {
 	checkErr(err)
 
 	b.Handle(tb.OnUserJoined, func(m *tb.Message) {
-		var msg = "\\[Gearheads\\]\nNew member\n\n👤"
-		var name = fmt.Sprintf("[%s", escapeMarkdownChar(m.UserJoined.FirstName))
-		if m.UserJoined.LastName != "" {
-			name = fmt.Sprintf("%s %s](tg://user?id=%s)", name, escapeMarkdownChar(m.UserJoined.LastName), m.UserJoined.Recipient())
-		} else {
-			name = fmt.Sprintf("%s](tg://user?id=%s)", name, m.UserJoined.Recipient())
+		log.Printf("OnUserJoined - %+v\n", m)
+
+		if m.Chat.ID == -1001090101913 {
+			fmt.Printf("%+v\n", m)
+
+			var msg = "\\[Gearheads\\]\nNew member\n\n👤"
+			var name = fmt.Sprintf("[%s", escapeMarkdownChar(m.UserJoined.FirstName))
+			if m.UserJoined.LastName != "" {
+				name = fmt.Sprintf("%s %s](tg://user?id=%s)", name, escapeMarkdownChar(m.UserJoined.LastName), m.UserJoined.Recipient())
+			} else {
+				name = fmt.Sprintf("%s](tg://user?id=%s)", name, m.UserJoined.Recipient())
+			}
+
+			if m.UserJoined.Username != "" {
+				msg = fmt.Sprintf("%s %s \\(@%s\\)", msg, name, m.UserJoined.Username)
+			}
+
+			msg = fmt.Sprintf("%s\n\\#new\\_member \\#c%s \\#u%s", msg, m.Chat.Recipient()[4:], m.UserJoined.Recipient())
+
+			b.Send(&tb.Chat{ID: config.IntegerFromSection("bot", "logchannel", 0)}, msg, &tb.SendOptions{ParseMode: tb.ModeMarkdownV2})
 		}
-
-		if m.UserJoined.Username != "" {
-			msg = fmt.Sprintf("%s %s \\(@%s\\)", msg, name, m.UserJoined.Username)
-		}
-
-		msg = fmt.Sprintf("%s\n\\#new\\_member \\#c%s \\#u%s", msg, m.Chat.Recipient()[4:], m.UserJoined.Recipient())
-
-		b.Send(&tb.Chat{ID: config.IntegerFromSection("bot", "logchannel", 0)}, msg, &tb.SendOptions{ParseMode: tb.ModeMarkdownV2})
 	})
 
 	b.Start()
