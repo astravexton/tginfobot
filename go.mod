a886d3fd4605bc8f6375af0efda6eed6751a6a29
Astra <squ1dge08@gmail.com>
2020-06-15 19:18:37 +0100

    Change ini package, add customizable welcome message

--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/astravexton/tginfobot
 go 1.14
 
 require (
-	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
-	github.com/sasbury/mini v0.0.0-20181226232755-dc74af49394b
-	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
-	gopkg.in/tucnak/telebot.v2 v2.0.0-20200426184946-59629fe0483e // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/smartystreets/goconvey v1.6.4 // indirect
+	gopkg.in/ini.v1 v1.57.0
+	gopkg.in/tucnak/telebot.v2 v2.3.1
 )
--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/sasbury/mini"
+	"gopkg.in/ini.v1"
 	tb "gopkg.in/tucnak/telebot.v2"
 )
 
@@ -17,44 +17,89 @@ func checkErr(e error) {
 }
 
 func main() {
-	config, err := mini.LoadConfiguration("config.ini")
+	config, err := ini.Load("config.ini")
+	checkErr(err)
+
+	owner, err := config.Section("bot").Key("owner").Int()
+	checkErr(err)
+
+	logchannel, err := config.Section("bot").Key("logchannel").Int64()
+	checkErr(err)
+
+	watchchannel, err := config.Section("bot").Key("channeltowatch").Int64()
 	checkErr(err)
 
 	b, err := tb.NewBot(tb.Settings{
-		Token:  config.StringFromSection("bot", "token", ""),
+		Token:  config.Section("bot").Key("token").String(),
 		Poller: &tb.LongPoller{Timeout: 10 * time.Second},
 	})
 
 	checkErr(err)
 
-	b.Handle(tb.OnUserJoined, func(m *tb.Message) {
-		if m.Chat.ID == -1001090101913 {
-
-			var msg = "\\[Gearheads\\]\nNew member\n\n👤"
-			var name = fmt.Sprintf("[%s", escapeMarkdownChar(m.UserJoined.FirstName))
-			if m.UserJoined.LastName != "" {
-				name = fmt.Sprintf("%s %s](tg://user?id=%s)", name, escapeMarkdownChar(m.UserJoined.LastName), m.UserJoined.Recipient())
-			} else {
-				name = fmt.Sprintf("%s](tg://user?id=%s)", name, m.UserJoined.Recipient())
-			}
-
-			msg = fmt.Sprintf("%s %s", msg, name)
-
-			if m.UserJoined.Username != "" {
-				msg = fmt.Sprintf("%s \\(@%s\\)", msg, m.UserJoined.Username)
-			}
-
-			msg = fmt.Sprintf("%s\n\\#new\\_member \\#c%s \\#u%s", msg, m.Chat.Recipient()[4:], m.UserJoined.Recipient())
-
-			b.Send(&tb.Chat{ID: config.IntegerFromSection("bot", "logchannel", 0)}, msg, &tb.SendOptions{ParseMode: tb.ModeMarkdownV2})
+	b.Handle("/setmessage", func(m *tb.Message) {
+		if m.Sender.ID == owner {
+			config.SaveTo("config.ini.bak")
+			config.Section("bot").Key("newusermessage").SetValue(strings.SplitN(m.Text, " ", 2)[1])
+			config.SaveTo("config.ini")
+			b.Send(m.Chat, "Saved")
 		}
 	})
+
+	b.Handle("/testmessage", func(m *tb.Message) {
+		b.Updates <- tb.Update{
+			Message: &tb.Message{
+				Chat: &tb.Chat{
+					ID:         -1001050101913,
+					Title:      "Chat Title",
+					Type:       tb.ChatGroup,
+					Username:   "testUsername",
+					InviteLink: "jiASOD8saHUDOSa",
+				},
+				UserJoined: &tb.User{
+					ID:        55258520,
+					Username:  "asstra",
+					FirstName: "Astra",
+					LastName:  "Vexton",
+				},
+			},
+		}
+	})
+
+	b.Handle(tb.OnUserJoined, func(m *tb.Message) {
+		if m.Chat.ID == watchchannel {
+
+			var msg = config.Section("bot").Key("newusermessage").String()
+			msg = formatMessage(m, msg)
+			b.Send(&tb.Chat{ID: logchannel}, msg, &tb.SendOptions{ParseMode: tb.ModeHTML})
+		}
+	})
+
+	fmt.Println("Bot running")
 
 	b.Start()
 
 }
 
 func escapeMarkdownChar(s string) string {
-	replacer := strings.NewReplacer("*", "\\*", "_", "\\_", "[", "\\[", "`", "\\`")
+	// replacer := strings.NewReplacer("*", "\\*", "_", "\\_", "[", "\\[", "`", "\\`")
+	replacer := strings.NewReplacer("<", "&lt;", ">", "&gt;", "&", "&amp;")
 	return replacer.Replace(s)
+}
+
+func formatMessage(data *tb.Message, msg string) (message string) {
+	firstLast := data.UserJoined.FirstName
+	if data.UserJoined.LastName != "" {
+		firstLast = fmt.Sprintf("%s %s", firstLast, data.UserJoined.LastName)
+	}
+
+	message = strings.Replace(msg, "$username", fmt.Sprintf("@%s", data.UserJoined.Username), -1)
+	message = strings.Replace(message, "$userid", data.UserJoined.Recipient(), -1)
+	message = strings.Replace(message, "$firstname", escapeMarkdownChar(data.UserJoined.FirstName), -1)
+	message = strings.Replace(message, "$lastname", escapeMarkdownChar(data.UserJoined.LastName), -1)
+	message = strings.Replace(message, "$firstlastname", escapeMarkdownChar(firstLast), -1)
+	message = strings.Replace(message, "$chatid", data.Chat.Recipient()[4:], -1)
+	message = strings.Replace(message, "$chattitle", data.Chat.Title, -1)
+	message = strings.Replace(message, "$chatinvitelink", fmt.Sprintf("tg://join?invite=%s", data.Chat.InviteLink), -1)
+
+	return
 }
